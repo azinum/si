28c8c11362704a6b850fe14d68ee7af714563a0a
azinum <lukaska61@gmail.com>
2020-05-19 21:57:00 +0200

    Cleaned up libbase

--- src/libbase.c
+++ src/libbase.c
@@ -10,25 +10,7 @@
 #include "api.h"
 #include "lib.h"
 
-int print_state(struct VM_state* vm, struct Scope* scope) {
-  if (!scope)
-    return 0;
-  printf("{\n");
-  for (int i = 0; i < ht_get_size(&scope->var_locations); i++) {
-    const Hkey* key = ht_lookup_key(&scope->var_locations, i);
-    const Hvalue* value = ht_lookup_byindex(&scope->var_locations, i);
-    if (key != NULL && value != NULL) {
-      printf("  %s: ", *key);
-      struct Object* object = &vm->variables[*value];
-      object_print(object);
-      printf(",\n");
-    }
-  }
-  printf("}\n");
-  return 0;
-}
-
-int print_recursive(struct VM_state* vm, struct Scope* scope, int level) {
+int print_state(struct VM_state* vm, struct Scope* scope, int level) {
   if (!scope)
     return 0;
   printf("{\n");
@@ -40,7 +22,7 @@ int print_recursive(struct VM_state* vm, struct Scope* scope, int level) {
       printf("  %s: ", *key);
       struct Object* object = &vm->variables[*value];
       if (object->type == T_FUNCTION) {
-        print_recursive(vm, &object->value.func.scope, level + 1);
+        print_state(vm, &object->value.func.scope, level + 1);
       }
       else {
         object_print(object);
@@ -68,13 +50,8 @@ static int base_print(struct VM_state* vm) {
   return 0;
 }
 
-static int base_print_global(struct VM_state* vm) {
-  print_state(vm, &vm->global.scope);
-  return 0;
-}
-
-static int base_print_recursive(struct VM_state* vm) {
-  print_recursive(vm, &vm->global.scope, 0);
+static int base_print_state(struct VM_state* vm) {
+  print_state(vm, &vm->global.scope, 0);
   return 0;
 }
 
@@ -89,8 +66,7 @@ static int base_assert(struct VM_state* vm) {
 
 static struct Lib_def baselib_funcs[] = {
   {"print", base_print},
-  {"print_global", base_print_global},
-  {"print_recursive", base_print_recursive},
+  {"print_state", base_print_state},
   {"assert", base_assert},
   {NULL, NULL},
 };
